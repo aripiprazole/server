ef9647d481a9a0c0fa09d4212568d400ecc42037
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-09 17:26:58 -0300

    Add UserDetailsServiceImpl with UserService

---
+++ src/main/java/store/wckd/server/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package store.wckd.server.auth;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import store.wckd.server.service.UserService;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        store.wckd.server.entity.User user = userService
+                .findByUsername(username)
+                .block();
+
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptySet()
+        );
+    }
+
+}
