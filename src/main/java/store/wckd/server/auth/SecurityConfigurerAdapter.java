8e6493f1d358b25eee3ea728c8f5ee6357432da1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 09:53:08 -0300

    Enable global authentication in SecurityConfigurerAdapter

--- src/main/java/store/wckd/server/auth/SecurityConfigurerAdapter.java
+++ src/main/java/store/wckd/server/auth/SecurityConfigurerAdapter.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +18,7 @@ import store.wckd.server.service.JwtService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalAuthentication
 public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
