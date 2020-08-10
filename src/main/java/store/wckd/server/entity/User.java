c7a063e7e58e02147833ea6f1c1675662990e00e
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 03:29:24 -0300

    Add setter functions to User columns

--- src/main/java/store/wckd/server/entity/User.java
+++ src/main/java/store/wckd/server/entity/User.java
@@ -3,12 +3,14 @@ package store.wckd.server.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
