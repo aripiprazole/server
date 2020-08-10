97e1218805c59afa285bbfc4583d8bc580c4de35
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 11:54:55 -0300

    Remove useless import from JwtFilter.java

--- src/main/java/store/wckd/server/auth/JwtFilter.java
+++ src/main/java/store/wckd/server/auth/JwtFilter.java
@@ -5,7 +5,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
