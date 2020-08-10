0156a95f077f5504c3625bd34b943591a3332ce7
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 08:41:21 -0300

    Add me controller to show user info for themself

---
+++ src/main/java/store/wckd/server/controller/MeController.java
@@ -0,0 +1,39 @@
+package store.wckd.server.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import store.wckd.server.entity.User;
+import store.wckd.server.service.UserService;
+
+@RestController
+public class MeController {
+
+    private final UserService userService;
+
+    public MeController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public Mono<User> me() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!(authentication instanceof UsernamePasswordAuthenticationToken))
+            throw new RuntimeException("Could not get the current session with that token");
+
+        UsernamePasswordAuthenticationToken credentialsToken = (UsernamePasswordAuthenticationToken) authentication;
+
+        Object usernameNullable = credentialsToken.getCredentials();
+        if(usernameNullable == null) usernameNullable = "";
+
+        String username = usernameNullable.toString();
+
+        return userService.findByUsername(username);
+    }
+
+}
