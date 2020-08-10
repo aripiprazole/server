1900a27662a94c994f738b31961751750b82aa1b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 04:03:41 -0300

    Add a user factory implementation

---
+++ src/test/java/store/wckd/server/factory/UserFactory.java
@@ -0,0 +1,31 @@
+package store.wckd.server.factory;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import store.wckd.server.entity.User;
+import store.wckd.server.repository.UserRepository;
+
+public class UserFactory implements Factory<User> {
+    private final UserRepository userRepository;
+
+    public UserFactory(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Flux<User> createMany(int amount) {
+        //noinspection ReactiveStreamsNullableInLambdaInTransform
+        return Flux
+                .range(1, amount)
+                .map(id -> createOne().block());
+    }
+
+    @Override
+    public Mono<User> createOne() {
+        return userRepository.save(new User(
+                "fake username",
+                "fake email",
+                "fake password"
+        ));
+    }
+}
