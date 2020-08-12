6922637fe36e3e98603ca2ef41b8ecd677e86c71
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 07:30:20 -0300

    Add login endpoint to api

--- src/main/java/store/wckd/server/controller/MeController.kt
+++ src/main/java/store/wckd/server/controller/MeController.kt
@@ -1,13 +1,41 @@
 package store.wckd.server.controller
 
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import store.wckd.server.dto.LoginRequestDTO
+import store.wckd.server.dto.LoginResponseDTO
 import store.wckd.server.dto.UserResponseDTO
 import store.wckd.server.entity.User
+import store.wckd.server.service.JwtService
+import store.wckd.server.service.UserService
 
 @RestController
-class MeController {
+class MeController(
+        private val userService: UserService,
+        private val jwtService: JwtService,
+        private val passwordEncoder: PasswordEncoder
+) {
+    @PostMapping("/login")
+    suspend fun login(@RequestBody body: LoginRequestDTO): ResponseEntity<*> {
+        val user: User = userService.findByUsername(body.username).awaitSingle()
+
+        if(!passwordEncoder.matches(body.password, user.password))
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(mapOf(
+                            "message" to "Invalid password or username."
+                    ))
+
+        return ResponseEntity.ok(LoginResponseDTO(jwtService.encodeJwt(user)))
+    }
+
     @GetMapping("/me")
     fun me(): UserResponseDTO {
         val authentication = SecurityContextHolder.getContext().authentication
---
+++ src/main/java/store/wckd/server/dto/LoginRequestDTO.kt
@@ -0,0 +1,6 @@
+package store.wckd.server.dto
+
+data class LoginRequestDTO(
+        val username: String,
+        val password: String
+)
