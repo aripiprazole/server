be0d0fb151873595fe3aa7e1cfed037b3076bf8f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 06:56:14 -0300

    Enable coroutine

--- src/main/java/store/wckd/server/ServerApplication.kt
+++ src/main/java/store/wckd/server/ServerApplication.kt
@@ -2,8 +2,10 @@ package store.wckd.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kotlin.coroutine.EnableCoroutine
 
 @SpringBootApplication
+@EnableCoroutine
 class ServerApplication
 
 fun main(vararg args: String) {
