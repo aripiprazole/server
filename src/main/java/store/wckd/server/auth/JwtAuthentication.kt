3a4e757400cc40ec7ec8c55646b1587ba0d9aa6e
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 12:13:27 -0300

    Add documentation for JwtAuthentication

--- src/main/java/store/wckd/server/auth/JwtAuthentication.kt
+++ src/main/java/store/wckd/server/auth/JwtAuthentication.kt
@@ -4,6 +4,16 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import store.wckd.server.entity.User
 
+/**
+ * Class used for session
+ *
+ * @param principal current session, could be null if the user is not logged,
+ *   could be called in a RestController with annotation @AuthenticationPrincipal
+ * @param credentials jwt token used/to be used to login
+ * @param name session name
+ * @param authorities scopes of the session
+ * @param isAuthenticated determine if the session is successfully authenticated
+ */
 data class JwtAuthentication(
         /** current user session */
         private val principal: User? = null,
@@ -11,7 +21,7 @@ data class JwtAuthentication(
         /** jwt token */
         private val credentials: String = "",
         private val name: String = "JwtAuthentication",
-        private val authorities: Collection<GrantedAuthority> = emptySet(),
+        private val authorities: Collection<GrantedAuthority> = emptySet(), // TODO: implement session scopes
         private val isAuthenticated: Boolean = false
 ) : Authentication {
     override fun getAuthorities() = authorities
@@ -22,6 +32,18 @@ data class JwtAuthentication(
 
     override fun isAuthenticated() = isAuthenticated
 
+    /**
+     * Nothing in this session is mutable, clone the session so then change the values:
+     *
+     * ```kotlin
+     * val session: JwtAuthentication = /* your session */
+     * val authenticatedSession = session.copy(
+     *   isAuthenticated = true
+     * )
+     * ```
+     *
+     * The same is supposed to be used when you change another field value.
+     */
     @Deprecated(
             message = "Copy the object to make any changes to make easier to manage the changes",
             replaceWith = ReplaceWith("jwtAuthentication.copy(isAuthenticated = true)")
