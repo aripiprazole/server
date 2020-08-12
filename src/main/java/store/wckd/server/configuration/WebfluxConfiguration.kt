140922a8cbf0d8ee3fe3b1b968b2ee5f6853d3ef
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 11:52:53 -0300

    Add configuration for swagger ui work

---
+++ src/main/java/store/wckd/server/configuration/WebfluxConfiguration.kt
@@ -0,0 +1,16 @@
+package store.wckd.server.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.ResourceHandlerRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class WebfluxConfiguration : WebFluxConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+}
