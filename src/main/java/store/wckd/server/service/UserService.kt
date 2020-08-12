3f5ce51cd1e9a2cc3c8a5f62cb5ae0ad4a9e9d41
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 12:05:45 -0300

    Add docs for user service

--- src/main/java/store/wckd/server/service/UserService.kt
+++ src/main/java/store/wckd/server/service/UserService.kt
@@ -9,12 +9,25 @@ import store.wckd.server.entity.User
 import store.wckd.server.exception.EntityNotFoundException
 import store.wckd.server.repository.UserRepository
 
+/**
+ * Class that provides users for you use
+ */
 @Service
 class UserService(private val userRepository: UserRepository) {
+    /**
+     * Find all paginated users in page number [page]
+     *
+     * @return a paginated result with size 15
+     */
     suspend fun findAll(page: Int): Page<User> {
         return userRepository.findAll(PageRequest.of(page, 15))
     }
 
+    /**
+     * Create a new user based on [userCreateDTO]
+     *
+     * @return the created user
+     */
     suspend fun create(userCreateDTO: UserCreateDTO): User {
         val user = User(
                 username = userCreateDTO.username,
@@ -25,14 +38,31 @@ class UserService(private val userRepository: UserRepository) {
         return userRepository.save(user)
     }
 
+    /**
+     * Find user by its username [username]
+     *
+     * @return the user found
+     * @throws EntityNotFoundException if could not found user
+     */
     suspend fun findByUsername(username: String): User {
         return userRepository.findByUsername(username).orElseThrow(::EntityNotFoundException)
     }
 
+    /**
+     * Find user by its id [id]
+     *
+     * @return the user found
+     * @throws EntityNotFoundException if could not found user
+     */
     suspend fun findById(id: Long): User {
         return userRepository.findById(id).orElseThrow(::EntityNotFoundException)
     }
 
+    /**
+     * Update by [id] or insert in database if do not exists based on [userUpdateDTO]
+     *
+     * @return the updated/created user
+     */
     suspend fun updateById(id: Long, userUpdateDTO: UserUpdateDTO): User {
         val user = User(
                 id,
@@ -44,6 +74,11 @@ class UserService(private val userRepository: UserRepository) {
         return userRepository.save(user)
     }
 
+    /**
+     * Delete user by its id [id]
+     *
+     * @return nothing
+     */
     suspend fun deleteById(id: Long) {
         userRepository.deleteById(id)
     }
