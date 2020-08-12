f62c86ec8f1c5dd71d6bbc623122157f67b85ddf
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 12:16:09 -0300

    Add documentation for UserRepository

--- src/main/java/store/wckd/server/repository/UserRepository.kt
+++ src/main/java/store/wckd/server/repository/UserRepository.kt
@@ -5,7 +5,15 @@ import org.springframework.stereotype.Repository
 import store.wckd.server.entity.User
 import java.util.*
 
+/**
+ * User JPA Repository. Provides users paginated.
+ */
 @Repository
 interface UserRepository : PagingAndSortingRepository<User, Long> {
+    /**
+     * Find a user by its username [username]
+     *
+     * @return if could not found a empty optional, else return the user optional-wrapped
+     */
     fun findByUsername(username: String): Optional<User>
 }
