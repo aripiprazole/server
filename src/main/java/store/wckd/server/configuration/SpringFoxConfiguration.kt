9d1d2fe1cbaefc61b323cce5561373092fc25e67
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 12:25:47 -0300

    Add documentation for Swagger Configuration

--- src/main/java/store/wckd/server/configuration/SpringFoxConfiguration.kt
+++ src/main/java/store/wckd/server/configuration/SpringFoxConfiguration.kt
@@ -8,6 +8,9 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+/**
+ * Sets up the swagger configurations
+ */
 @Configuration
 @EnableSwagger2
 class SpringFoxConfiguration {
